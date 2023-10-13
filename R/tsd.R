81a42b7b1bd3899b59b2b7d8643c72c21fe20e74
telkamp7 <kasper.telkamp@gmail.com>
2023-10-13 08:48:06 +0200

    added a new function, tsd, to structure the input data

---
+++ R/tsd.R
@@ -0,0 +1,44 @@
+#' Create a tsibble (time-series data) object from observed data and corresponding dates.
+#'
+#' This function takes observed data and the corresponding date vector and converts it into a tsibble object,
+#' which is a time series data structure that can be used for time series analysis.
+#'
+#' @param observed A numeric vector containing the observations.
+#' @param time A date vector containing the corresponding dates.
+#' @param time_interval A character vector specifying the time interval, choose between "day," "week," or "month."
+#'
+#' @return A tsibble object containing time, the observations, and the periodInYear.
+#'
+#' @export
+#'
+#' @examples
+#' # Create a tsibble object from daily data
+#' daily_tsd <- tsd(observed = c(10, 15, 20, 18), time = as.Date(c("2023-01-01", "2023-01-02", "2023-01-03", "2023-01-04")), time_interval = "day")
+#'
+#' # Create a tsibble object from weekly data
+#' weekly_tsd <- tsd(observed = c(100, 120, 130), time = as.Date(c("2023-01-01", "2023-01-08", "2023-01-15")), time_interval = "week")
+#'
+#' # Create a tsibble object from monthly data
+#' monthly_tsd <- tsd(observed = c(500, 520, 540), time = as.Date(c("2023-01-01", "2023-02-01", "2023-03-01")), time_interval = "month")
+#'
+tsd <- function(observed, time, time_interval = c("day", "week", "month")){
+
+  # Throw an error if any of the inputs are not supported
+  time_interval <- rlang::arg_match(time_interval)
+
+  # Select the correct 'time_interval' and create the 'tsd' return object
+  ans <- switch(
+    time_interval,
+    day = tibble(time = time, observed = observed) %>%
+      dplyr::mutate(periodInYear = lubridate::yday(time)) %>%
+      tsibble::as_tsibble(index = time),
+    week = tibble(time = tsibble::yearweek(time), observed = observed) %>%
+      dplyr::mutate(periodInYear = lubridate::isoweek(time)) %>%
+      tsibble::as_tsibble(index = time),
+    month = tibble(time = tsibble::yearmonth(time), observed = observed) %>%
+      dplyr::mutate(periodInYear = lubridate::month(time)) %>%
+      tsibble::as_tsibble(index = time)
+  )
+
+  return(ans)
+}
